a0764b06884a1b3c9a63c73474077b12ac5e33d1
lmnt1 <sb@lmnt.one>
2023-02-23 07:58:05 +0100

    Codemaid

--- MagneticTool/Platforms/MacCatalyst/AppDelegate.cs
+++ MagneticTool/Platforms/MacCatalyst/AppDelegate.cs
@@ -5,5 +5,5 @@ namespace MagneticTool;
 [Register("AppDelegate")]
 public class AppDelegate : MauiUIApplicationDelegate
 {
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
